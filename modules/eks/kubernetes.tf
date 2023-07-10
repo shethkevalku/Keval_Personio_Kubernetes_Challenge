c190e1c8e5a9a6d354aeb7d586288fdc73e185ad
328296826261 <skevalku@amazon.com>
2023-07-10 16:32:54 +0100

    eks and ecr config15

--- modules/eks/kubernetes.tf
+++ modules/eks/kubernetes.tf
@@ -25,7 +25,7 @@ resource "kubernetes_deployment" "app1" {
           name  = "test-sample-website"
 
           port {
-            container_port = 80
+            container_port = 8080
           }
 
           resources {
