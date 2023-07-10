82fed5a50f8069ae34cc1052d749f86309209cee
shethkevalku <105679652+shethkevalku@users.noreply.github.com>
2023-07-10 15:29:20 +0100

    Update eks.tf

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -121,9 +121,10 @@ data "terraform_remote_state" "eks" {
 
 provider "kubernetes" {
   host                   = aws_eks_cluster.personio-eks.endpoint
+  config_path            = "~/.kube/config"
   cluster_ca_certificate = base64decode(aws_eks_cluster.personio-eks.certificate_authority.0.data)
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     args = [
       "eks",
