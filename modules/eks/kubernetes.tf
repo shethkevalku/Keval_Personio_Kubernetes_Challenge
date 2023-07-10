9258310378a2b28699e3d4a486d161ea6f1db855
328296826261 <skevalku@amazon.com>
2023-07-10 17:50:38 +0100

    eks automation

--- modules/eks/kubernetes.tf
+++ modules/eks/kubernetes.tf
@@ -1,62 +1,62 @@
-resource "kubernetes_deployment" "app1" {
-  metadata {
-    name = "test-sample-website"
-    labels = {
-      App = "test-sample-website"
-    }
-  }
+# resource "kubernetes_deployment" "app1" {
+#   metadata {
+#     name = "test-sample-website"
+#     labels = {
+#       App = "test-sample-website"
+#     }
+#   }
 
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        App = "test-sample-website"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          App = "test-sample-website"
-        }
-      }
-      spec {
-        container {
-          image = "328296826261.dkr.ecr.us-east-2.amazonaws.com/personio-ecr:development"
-          name  = "test-sample-website"
+#   spec {
+#     replicas = 2
+#     selector {
+#       match_labels = {
+#         App = "test-sample-website"
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           App = "test-sample-website"
+#         }
+#       }
+#       spec {
+#         container {
+#           image = "328296826261.dkr.ecr.us-east-2.amazonkubectl aws.com/personio-ecr:development"
+#           name  = "test-sample-website"
 
-          port {
-            container_port = 8080
-          }
+#           port {
+#             container_port = 8080
+#           }
 
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-        }
-      }
-    }
-  }
-}
-resource "kubernetes_service_v1" "example" {
-  metadata {
-    name = "test-sample-website-service"
-  }
-  spec {
-    selector = {
-      app = "test-sample-website"
-    }
-    port {
-      protocol    = "TCP"
-      port        = 80
-      target_port = 8080
-    }
+#           resources {
+#             limits = {
+#               cpu    = "0.5"
+#               memory = "512Mi"
+#             }
+#             requests = {
+#               cpu    = "250m"
+#               memory = "50Mi"
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+# resource "kubernetes_service_v1" "example" {
+#   metadata {
+#     name = "test-sample-website-service"
+#   }
+#   spec {
+#     selector = {
+#       app = "test-sample-website"
+#     }
+#     port {
+#       protocol    = "TCP"
+#       port        = 80
+#       target_port = 8080
+#     }
 
-    type = "LoadBalancer"
-  }
-}
+#     type = "LoadBalancer"
+#   }
+# }
