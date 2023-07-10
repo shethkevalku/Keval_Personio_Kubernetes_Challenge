bd1f7b997dedbe35fe2aec84d890113c90faedc6
328296826261 <skevalku@amazon.com>
2023-07-10 12:18:28 +0100

    initial commit and vpc creation in us-west-2

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  availability_zones = ["us-east-1a", "us-east-1b"]
+  availability_zones = ["us-east-2a", "us-east-2b"]
 
   vpc_cidr             = "172.16.0.0/16"
   public_subnet_cidrs  = ["172.16.0.0/24", "172.16.1.0/24"]
